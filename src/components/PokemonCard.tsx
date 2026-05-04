4ce4af800ff8432d87ff38b3aa82aedca9cbb002
Coline Rabemihoatra <coline.rbm@gmail.com>
2026-05-04 15:52:47 +0200

    update

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -9,8 +9,8 @@ function PokemonCard({ pokemon }: PokemonCardProps) {
   return (
     <section>
       {pokemon.imgSrc ? (
-        <figure>
-          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        <figure className="card">
+          <img className="card-img" src={pokemon.imgSrc} alt={pokemon.name} />
           <figcaption>{pokemon.name}</figcaption>
         </figure>
       ) : (
